1a6a38fc52306bf71e0d7caddc286ab82a6efa04
Michal Bartosik <msbartosik@gmail.com>
2021-04-20 18:39:44 +0200

    padding adjustments for logo

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 const form = document.querySelector(".js-form");
 const input = document.querySelector(".js-email");
 const parent = document.querySelector(".js-parent");
+const msg = document.querySelector(".js-error-msg")
 
 const checkEmail = (email) => {
   const re = /^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/;
@@ -9,9 +10,20 @@ const checkEmail = (email) => {
 
 const handleSubmit = (e) => {
   e.preventDefault();
-  if (!checkEmail(input.value)) {
-    parent.classList.add('is-error')
+  let errorMsg = ''
+
+  if (!input.value) {
+    errorMsg= 'Oops! Please add your email'
+  } else if (!checkEmail(input.value)) {
+    errorMsg = 'Oops! That doesn\'t look like an email address'
   }
+
+  if (!errorMsg) {
+    return;
+  }
+
+  parent.classList.add('is-error');
+  msg.innerText = errorMsg;
 };
 
 const handleInput = () => {
